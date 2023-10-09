e30395ab8275817b2f6bb193f09424c506c36c2a
kevocodes <00116120@uca.edu.sv>
2023-10-09 00:20:08 -0600

    feat: update post strings validation :sparkles:

--- src/post/dtos/post.dto.ts
+++ src/post/dtos/post.dto.ts
@@ -5,13 +5,13 @@ import { IsString, IsUrl, MaxLength, MinLength } from 'class-validator';
 export class CreatePostDto {
   @IsString()
   @Transform(({ value }) => value.trim())
-  @MinLength(8)
+  @MinLength(1)
   @MaxLength(60)
   title: string;
 
   @IsString()
   @Transform(({ value }) => value.trim())
-  @MinLength(8)
+  @MinLength(1)
   description: string;
 
   @IsUrl()
@@ -23,6 +23,6 @@ export class UpdatePostDto extends PartialType(CreatePostDto) {}
 export class CommentPostDto {
   @IsString()
   @Transform(({ value }) => value.trim())
-  @MinLength(8)
+  @MinLength(1)
   content: string;
 }
