6d5c0dfd0ec85f5ea77b8c2e88b448a66a6106af
kevocodes <00116120@uca.edu.sv>
2023-11-02 16:55:20 -0600

    refactor: remove title prop to post schema :recycle:

--- src/posts/dtos/post.dto.ts
+++ src/posts/dtos/post.dto.ts
@@ -1,14 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsString, IsUrl, MaxLength, MinLength } from 'class-validator';
+import { IsString, IsUrl, MinLength } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
-  @Transform(({ value }) => value.trim())
-  @MinLength(1)
-  @MaxLength(60)
-  title: string;
-
   @IsString()
   @Transform(({ value }) => value.trim())
   @MinLength(1)
--- src/posts/schemas/post.schema.ts
+++ src/posts/schemas/post.schema.ts
@@ -7,9 +7,6 @@ export type PostDocument = HydratedDocument<Post>;
 @Schema({ timestamps: true })
 export class Post {
   @Prop({ required: true, trim: true })
-  title: string;
-
-  @Prop({ required: true, trim: true })
   description: string;
 
   @Prop({ required: true, trim: true })
